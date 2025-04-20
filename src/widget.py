881a6ec2c6c6d9b44ed6cf8a1431f536b6165e95
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-04-20 16:56:22 +0300

    second commit. added module widget.py

---
+++ src/widget.py
@@ -0,0 +1,44 @@
+from src.masks import (get_mask_account,get_mask_card_number)
+
+def mask_account_card(type_and_number: str) -> str:
+    """Функция принимает один аргумент — строку, содержащую тип и номер карты или счета.
+    Возвращает строку с замаскированным номером."""
+
+    # Делит входную строку на список
+    str_split = type_and_number.split()
+
+    # Далее проверка элементов списка. Если первый элемент равен "счет",
+    # то последний элемент списка передается в функцию для номера счета,
+    # иначе в функцию для номера карты.
+    if str_split[0].lower() == "счет" or str_split[0].lower() == "счёт":
+        masked_number = get_mask_account(str_split[-1])
+        result = [str_split[0], masked_number]
+        return " ".join(result)
+
+    masked_card = get_mask_card_number(str_split[-1])
+    del str_split[-1]
+    result = str_split + [masked_card]
+    return " ".join(result)
+
+
+def get_date(str_data: str) -> str:
+    """Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")."""
+
+    correct_str_data = str_data[:10]
+    correct_data_list = correct_str_data.split("-") # разделяю по "-" на список
+    correct_data_list.reverse() # переворачивает элементы списка
+    return ".".join(correct_data_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
