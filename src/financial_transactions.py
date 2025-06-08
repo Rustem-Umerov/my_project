2251dea98684675dbf0d3e8e40b721863bb5bbb1
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-06-08 23:53:38 +0300

    Wrote functions _detect_delimiter() and fin_trans_from_csv_file() for reading financial transactions from a CSV file.

---
+++ src/financial_transactions.py
@@ -0,0 +1,68 @@
+import csv
+from pathlib import Path
+
+from src.logger_config import get_logger
+
+logger = get_logger(__name__)
+
+my_project_dir = Path(__file__).resolve().parent.parent
+csv_file_path = my_project_dir / "files_with_financ_transactions" / "transactions.csv"
+
+
+def _detect_delimiter(file_path: Path, sample_size: int = 1024) -> str:
+    """Функция определяет разделитель в CSV-файле с помощью csv.Sniffer"""
+
+    try:
+        logger.info("Анализ разделителя в файле: %s", file_path)
+        with open(file_path, "r", encoding="utf-8") as f:
+            sample = f.read(sample_size)
+            logger.info("Образец данных из файла прочитан (первые %s байт).", sample_size)
+
+            # Ниже создаю объект sniffer класса csv.Sniffer()
+            sniffer = csv.Sniffer()
+            # Вызываю метод sniff(): Метод анализирует строку sample, учитывая список кандидатов на разделители ;,\t|
+            dialect = sniffer.sniff(sample, delimiters=";,\t|")
+            # Ниже извлекаю найденный разделитель
+            detected_delimiter = dialect.delimiter
+            logger.info("Определен разделитель: %s", detected_delimiter)
+
+            return detected_delimiter
+
+    except FileNotFoundError:
+        logger.error("Файл %s не найден!", file_path)
+        raise
+    except Exception as e:
+        logger.error("Неизвестная ошибка при чтении %s: %s", file_path, str(e))
+        raise
+
+
+def fin_trans_from_csv_file(file_path: Path) -> list[dict]:
+    """Функция для считывания финансовых операций из CSV-файла.
+    Принимает путь к файлу CSV в качестве аргумента.
+    Возвращает список словарей с транзакциями."""
+
+    try:
+        logger.info("Происходит открытие CSV-файла: %s", file_path)
+        delimiter = _detect_delimiter(file_path)
+
+        with open(file_path, encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=delimiter)
+
+            logger.info("CSV-файл успешно открыт. Информация из файла получена.")
+            transactions = list(reader)
+
+            if not transactions:
+                logger.info("Файл %s не содержит данных", file_path)
+                raise ValueError("Файл %s не содержит данных", file_path)
+
+            return transactions
+
+    except FileNotFoundError:
+        logger.error("Файл %s не найден!", file_path)
+        raise
+    except csv.Error as e:
+        logger.error("Ошибка в CSV-файле %s: %s", file_path, str(e))
+        raise
+    except Exception as e:
+        logger.error("Неизвестная ошибка при чтении %s: %s", file_path, str(e))
+        raise
