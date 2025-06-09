cf8c7f1e33b36fab91a61a8591c20544601d8eb2
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-06-09 15:56:06 +0300

    rote the fin_trans_from_excel_file() function to read financial transactions from an xcel file.

--- src/financial_transactions.py
+++ src/financial_transactions.py
@@ -9,6 +9,7 @@ logger = get_logger(__name__)
 
 my_project_dir = Path(__file__).resolve().parent.parent
 csv_file_path = my_project_dir / "files_with_financ_transactions" / "transactions.csv"
+excel_file_path = my_project_dir / "files_with_financ_transactions" / "transactions_excel.xlsx"
 
 
 def _detect_delimiter(file_path: Path, sample_size: int = 1024) -> str:
@@ -63,6 +64,35 @@ def fin_trans_from_csv_file(file_path: Path) -> list[dict]:
         raise
     except pd.errors.ParserError as e:
         logger.error("Ошибка в CSV-файле %s: %s", file_path, str(e))
+        raise
+    except Exception as e:
+        logger.error("Неизвестная ошибка при чтении %s: %s", file_path, str(e))
+        raise
+
+
+def fin_trans_from_excel_file(file_path: Path) -> list[dict]:
+    """Функция для считывания финансовых операций из Excel-файла.
+    Принимает путь к файлу Excel-файлу в качестве аргумента.
+    Возвращает список словарей с транзакциями."""
+
+    try:
+        logger.info("Происходит открытие Excel-файла: %s", file_path)
+
+        excel_data = pd.read_excel(file_path)
+        logger.info("Excel-файл успешно открыт. Информация из файла получена.")
+        transactions_list = excel_data.to_dict(orient="records")
+
+        if not transactions_list:
+            logger.info("Файл %s не содержит данных", file_path)
+            raise ValueError("Файл %s не содержит данных", file_path)
+
+        return transactions_list
+
+    except FileNotFoundError:
+        logger.error("Файл %s не найден!", file_path)
+        raise
+    except pd.errors.ParserError as e:
+        logger.error(f"Ошибка при парсинге Excel-файла {file_path}: {str(e)}")
         raise
     except Exception as e:
         logger.error("Неизвестная ошибка при чтении %s: %s", file_path, str(e))
