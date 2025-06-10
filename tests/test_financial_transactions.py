33c1398252f548ad891874ecb824134472595223
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-06-10 08:47:29 +0300

    wrote tests for the fin_trans_from_csv_file() function

--- tests/test_financial_transactions.py
+++ tests/test_financial_transactions.py
@@ -2,13 +2,15 @@ from pathlib import Path
 from typing import Any
 from unittest.mock import Mock, mock_open, patch
 
+import pandas as pd
 import pytest
+from pandas.errors import ParserError
 from pytest import LogCaptureFixture
 
-from src.financial_transactions import detect_delimiter
+from src.financial_transactions import detect_delimiter, fin_trans_from_csv_file
 
 
-def fake_sniff(_sample: str, _delimiters: str) -> Any:
+def fake_sniff(sample: str, delimiters: str) -> Any:
     """
     Функция, которая заменяет csv.Sniffer.sniff().
     Она возвращает mock-объект с атрибутом delimiter, равным ';'
@@ -18,7 +20,7 @@ def fake_sniff(_sample: str, _delimiters: str) -> Any:
     return mock_dialect
 
 
-def test__detect_delimiter_correct_delimiter() -> None:
+def test_detect_delimiter_correct_delimiter() -> None:
     """Тестирует успешное определение разделителя."""
 
     # Ниже пример файла для теста. Данные с разделителем ";".
@@ -52,7 +54,7 @@ def test__detect_delimiter_correct_delimiter() -> None:
         "Определен разделитель:",
     ],
 )
-def test__detect_delimiter_correct_delimiter_log_message(caplog: LogCaptureFixture, log_message: str) -> None:
+def test_detect_delimiter_correct_delimiter_log_message(caplog: LogCaptureFixture, log_message: str) -> None:
     """Тест проверяет логирование ошибок."""
 
     fake_data = "apple1;apple2;apple3\napple4;apple5;apple6\napple7;apple8;apple9"
@@ -63,7 +65,7 @@ def test__detect_delimiter_correct_delimiter_log_message(caplog: LogCaptureFixtu
     assert log_message in caplog.text
 
 
-def test__detect_delimiter_file_not_found(caplog: LogCaptureFixture) -> None:
+def test_detect_delimiter_file_not_found(caplog: LogCaptureFixture) -> None:
     """
     Тест тестирует ситуацию, когда файл не найден.
 
@@ -81,7 +83,7 @@ def test__detect_delimiter_file_not_found(caplog: LogCaptureFixture) -> None:
         assert "Файл", "не найден!" in caplog.text
 
 
-def test__detect_delimiter_except_exception(caplog: LogCaptureFixture) -> None:
+def test_detect_delimiter_except_exception(caplog: LogCaptureFixture) -> None:
     """Тест проверят, что функция корректно обрабатывает исключение Exception."""
 
     fake_data = "не правильные данные, для того чтобы было вызвано except Exception as e"
@@ -96,4 +98,106 @@ def test__detect_delimiter_except_exception(caplog: LogCaptureFixture) -> None:
             detect_delimiter(fake_path)
 
     assert "Error: fake error for testing" in str(exception_info.value)
+    assert "Неизвестная ошибка при чтении" in caplog.text
+
+
+@pytest.mark.parametrize(
+    "log_message",
+    [
+        "Происходит открытие CSV-файла:",
+        "CSV-файл успешно открыт. Информация из файла получена.",
+    ],
+)
+def test_fin_trans_from_csv_file_correct_file_and_log_message(caplog: LogCaptureFixture, log_message: str) -> None:
+    """Тест проверяет:
+    - корректное выполнение функции при корректном считывании CSV-файла.
+    - правильный вывод логов."""
+
+    # Ниже список словарей для теста.
+    fake_data = [
+        {"fruit1": "apple1", "fruit2": "apple2", "fruit3": "apple3"},
+        {"fruit1": "apple4", "fruit2": "apple5", "fruit3": "apple6"},
+        {"fruit1": "apple7", "fruit2": "apple8", "fruit3": "apple9"},
+    ]
+
+    fake_path = Path("fake_transactions.csv")  # имитация пути к файлу.
+    expected_delimiter = ";"  # ожидаемый разделитель.
+
+    fake_data_frame = pd.DataFrame(fake_data)  # преобразую список словарей в объект DataFrame.
+    expected_result = fake_data_frame.to_dict(orient="records")  # перевожу объект DataFrame в список.
+
+    with (
+        patch("src.financial_transactions.detect_delimiter", return_value=expected_delimiter) as mock_detect,
+        patch("src.financial_transactions.pd.read_csv", return_value=fake_data_frame) as mock_read_csv,
+        patch("src.financial_transactions.validate_transactions_list") as mock_validate,
+    ):
+        result = fin_trans_from_csv_file(fake_path)  # вызов функции.
+
+        assert result == expected_result  # проверка результата.
+
+        # ниже проверка того, что каждая функция вызывалась с нужными параметрами.
+        mock_detect.assert_called_once_with(fake_path)
+        mock_read_csv.assert_called_once_with(fake_path, sep=expected_delimiter, encoding="utf-8")
+        mock_validate.assert_called_once_with(fake_data, fake_path)
+
+        assert log_message in caplog.text  # проверка логов.
+
+
+def test_fin_trans_from_csv_file_file_not_found(caplog: LogCaptureFixture) -> None:
+    """
+    Тест проверяет, что при отсутствии файла функция:
+    - регистрирует ошибку и выбрасывает исключение FileNotFoundError.
+    - выводит правильное логирование."""
+
+    fake_path = Path("fake_transactions.csv")  # имитация пути к файлу.
+    expected_delimiter = ";"  # ожидаемый разделитель.
+
+    with (
+        patch("src.financial_transactions.detect_delimiter", return_value=expected_delimiter),
+        patch("src.financial_transactions.pd.read_csv", side_effect=FileNotFoundError("File not found")),
+        pytest.raises(FileNotFoundError) as exc_info,
+    ):
+
+        fin_trans_from_csv_file(fake_path)
+
+    assert "File not found" in str(exc_info)
+
+    assert "Файл", "не найден!" in caplog.text
+
+
+def test_fin_trans_from_csv_file_parser_error(caplog: LogCaptureFixture) -> None:
+    """
+    Тест проверяет, что при возникновении ошибки парсинга CSV функция:
+    - регистрирует ошибку и выбрасывает исключение pd.errors.ParserError.
+    - выводит правильное логирование"""
+    fake_path = Path("fake_transactions.csv")
+
+    with (
+        patch("src.financial_transactions.detect_delimiter", return_value=";"),
+        patch("src.financial_transactions.pd.read_csv", side_effect=ParserError("Ошибка парсинга")),
+    ):
+        with pytest.raises(ParserError) as exc_info:
+            fin_trans_from_csv_file(fake_path)
+
+    assert "Ошибка парсинга" in str(exc_info.value)
+    assert "Ошибка в CSV-файле" in caplog.text
+
+
+def test_fin_trans_from_csv_file_unknown_exception(caplog: LogCaptureFixture) -> None:
+    """
+    Тест проверяет поведение функции при возникновении неизвестной ошибки:
+    - вызывается общее исключение Exception.
+    - проверяется, что ошибка залогирована.
+    """
+    fake_path = Path("fake_transactions.csv")
+
+    with (
+        patch("src.financial_transactions.detect_delimiter", return_value=";"),
+        patch("src.financial_transactions.pd.read_csv", side_effect=Exception("Неожиданная ошибка")),
+    ):
+        with pytest.raises(Exception) as exc_info:
+            fin_trans_from_csv_file(fake_path)
+
+    assert "Неожиданная ошибка" in str(exc_info.value)
+    # Если в логах присутствует фраза "Неизвестная ошибка при чтении", проверим её
     assert "Неизвестная ошибка при чтении" in caplog.text
