db4fb591921bb616d7e6fe0fb116d4287f9d46bc
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-06-14 12:40:15 +0300

    wrote tests for the process_bank_operations() function

--- tests/test_banking_operations.py
+++ tests/test_banking_operations.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.banking_operations import process_bank_search
+from src.banking_operations import process_bank_search, process_bank_operations
 
 
 @pytest.mark.parametrize(
@@ -41,3 +41,90 @@ def test_process_bank_search_invalid_data(
 
     result = process_bank_search(transaction_invalid_data, search_str)
     assert result == result_list
+
+
+@pytest.mark.parametrize(
+    "not_list, input_str",
+    [
+        ("abc", "Открытие вклада"),
+        (123, "Открытие вклада"),
+        (({"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}), "Открытие вклада"),
+    ],
+)
+def test_process_bank_search_not_list(not_list: list, input_str: str) -> None:
+    """Тест проверяет, что если будет не список, а другой тип данных, будет ошибка ValueError."""
+
+    with pytest.raises(ValueError):
+        process_bank_search(not_list, input_str)
+
+
+@pytest.mark.parametrize(
+    "input_list, not_str",
+    [
+        ([{"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}], ["Открытие вклада"]),
+        ([{"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}], ("Открытие вклада",)),
+        ([{"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}], 123),
+    ],
+)
+def test_process_bank_search_not_str(input_list: list, not_str: str) -> None:
+    """Тест проверяет, что если будет не строка, а другой тип данных, будет ошибка ValueError."""
+
+    with pytest.raises(ValueError):
+        process_bank_search(input_list, not_str)
+
+
+@pytest.mark.parametrize(
+    "categories_list, result_dict",
+    [
+        (["Перевод организации"], {"Перевод организации": 2}),
+        (["Перевод организации", "Открытие вклада"], {"Перевод организации": 2, "Открытие вклада": 2}),
+        (["Перевод с карты на карту"], {"Перевод с карты на карту": 2}),
+        (
+            ["Перевод с карты на карту", "Перевод со счета на счет"],
+            {"Перевод с карты на карту": 2, "Перевод со счета на счет": 2},
+        ),
+        (["abc", "cba"], {}),
+        ([1234, 1234], {}),
+    ],
+)
+def test_process_bank_operations(transaction_data: list[dict], categories_list: list, result_dict: dict) -> None:
+    """Тест проверяет, что функция правильно работает, в разных ситуациях."""
+
+    result = process_bank_operations(transaction_data, categories_list)
+    assert result == result_dict
+
+
+#########
+
+
+@pytest.mark.parametrize(
+    "not_list, input_list",
+    [
+        ("abc", ["Открытие вклада", "Перевод со счета на счет"]),
+        (123, ["Открытие вклада", "Перевод со счета на счет"]),
+        (
+            ({"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}),
+            ["Открытие вклада", "Перевод со счета на счет"],
+        ),
+    ],
+)
+def test_process_bank_operations_transaction_data_not_list(not_list: list, input_list: list) -> None:
+    """Тест проверяет, что если будет не список, а другой тип данных, будет ошибка ValueError."""
+
+    with pytest.raises(ValueError):
+        process_bank_operations(not_list, input_list)
+
+
+@pytest.mark.parametrize(
+    "input_list, not_list",
+    [
+        ([{"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}], "Открытие вклада"),
+        ([{"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}], ("Открытие вклада",)),
+        ([{"description": "Перевод со счета на счет"}, {"description": "Открытие вклада"}], 123),
+    ],
+)
+def test_process_bank_operations_categories_not_list(input_list: list, not_list: list) -> None:
+    """Тест проверяет, что если будет не список, а другой тип данных, будет ошибка ValueError."""
+
+    with pytest.raises(ValueError):
+        process_bank_operations(input_list, not_list)
