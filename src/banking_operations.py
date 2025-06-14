c39f35f50c2b6ec2739f92ae1a8f08bf280211bf
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-06-14 12:38:34 +0300

    improved functions: added condition checking input data

--- src/banking_operations.py
+++ src/banking_operations.py
@@ -6,6 +6,11 @@ def process_bank_search(banking_transaction_data: list[dict], search: str) -> li
     """Функция, принимает на вход, список словарей с данными о банковских операциях и строку для поиска.
     Возвращает список словарей, с данными о банковских операциях, у которых в описании есть данная строка."""
 
+    if not isinstance(banking_transaction_data, list):
+        raise ValueError(f"Expected a list, but received {type(banking_transaction_data)}")
+    if not isinstance(search, str):
+        raise ValueError(f"Expected a string, but received {type(search)}")
+
     pattern = re.compile(search, re.IGNORECASE)
     result_list = []
 
@@ -24,7 +29,12 @@ def process_bank_operations(banking_transaction_data: list[dict], categories: li
     а возвращает словарь, в котором ключи — это названия категорий,
     а значения — это количество операций в каждой категории."""
 
-    categories_set = {category.lower() for category in categories}
+    if not isinstance(banking_transaction_data, list):
+        raise ValueError(f"Expected a list, but received {type(banking_transaction_data)}")
+    if not isinstance(categories, list):
+        raise ValueError(f"Expected a list, but received {type(categories)}")
+
+    categories_set = {category.lower() for category in categories if isinstance(category, str)}
     categories_list = []
 
     for dict_transaction in banking_transaction_data:
